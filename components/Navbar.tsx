ba83f9635e52ef1c0ddf9090328e07e3bdfb616c
Ricardo Otálora Luna <ruperto1@protonmail.com>
2022-10-02 18:21:12 -0500

    logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,8 @@ import Logo from '../assets/logo.png'
 const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-gray-900 text-gray-300">
-      <div>
-        <Image src={Logo} alt="Logo Image" height={50} width={50}/>
-        
+      <div className='pt-5'>
+        <Image src={Logo} alt="Logo Image" height={200} width={200}/>
       </div>
     </div>
   )
